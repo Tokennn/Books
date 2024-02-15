0323e376c7774a6ac598b3c2f9f619ccbc825689
coquentin <quentin.contreau@ynov.com>
2024-02-15 12:46:08 +0100

    Front beginning

--- src/main/java/com/example/librarybook/Appli.java
+++ src/main/java/com/example/librarybook/Appli.java
@@ -1,40 +1,41 @@
 package com.example.librarybook;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.concurrent.ConcurrentMap;
+
 public class Appli extends Application {
+
+    @FXML
+    private Button updateButton;
+    @FXML
+    private Button addButton;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private ComboBox filterComboBox;
+    @FXML
+    private TableView livresTable;
+    @FXML
+    private Button reviseButton;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Création de l'interface graphique
-        VBox root = new VBox();
-        Scene scene = new Scene(root, 600, 400);
-
-        // Création des composants
-        TableView<Livre> livresTable = new TableView<>();
-        TextField searchBar = new TextField();
-        Button addButton = new Button("Ajouter");
-        Button deleteButton = new Button("Supprimer");
-        Button updateButton = new Button("Mettre à jour");
-        Button clearButton = new Button("Effacer");
-        Button reviseButton = new Button("Réviser");
-        ComboBox<String> filterComboBox = new ComboBox<>();
-        // Ajoutez les options pour filtrer
-        filterComboBox.getItems().addAll("Filtrer par genre", "Filtrer par année de sortie", "Filtrer par auteur");
-
-        // Ajouter les composants à la racine
-        root.getChildren().addAll(searchBar, addButton, deleteButton, updateButton, clearButton, reviseButton, filterComboBox, livresTable);
-
-        // Affichage de la fenêtre
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("livre.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Gestion de bibliothèque");
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Library");
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
